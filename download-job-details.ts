19fa954a09b9574829817f95a83b93897db449ae
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-13 17:48:55 +0000

    removed commented out code
    added redirect functionality

--- download-job-details.ts
+++ download-job-details.ts
@@ -7,7 +7,6 @@ export class DownladJobDetails {
     private readonly jobDetailsContent: {url: string, content: string}[]
 
     constructor(jobUrlsFile: string, private readonly jobDetailContentFile: string) {
-        
         this.urls = existsSync(jobUrlsFile) ? JSON.parse(readFileSync(jobUrlsFile, 'utf8')) : []
         this.jobDetailsContent = existsSync(jobDetailContentFile) ? JSON.parse(readFileSync(jobDetailContentFile, 'utf8')) : []
     }
@@ -16,10 +15,18 @@ export class DownladJobDetails {
         return new Promise<void>(async (resolve, reject) => {
             let saveChanges = false
             for(const url of this.urls) {
-                const jobDownloaded = this.jobDetailsContent.filter(x => x.url === url).length > 0
+                let jobDownloaded = this.jobDetailsContent.filter(x => x.url === url).length > 0
                 if (!jobDownloaded) {
                     const content = await request(url)
-                    this.jobDetailsContent.push({url, content})   
+                    if(typeof(content) === 'string') 
+                        this.jobDetailsContent.push({url, content})
+                    else if (typeof(content) === 'object') {
+                        const v = content as {url: string, data: string, location: string} 
+                        if (!(this.jobDetailsContent.filter(x => x.url === v.location).length > 0)) {
+                            const c = await request(v.location) as string
+                            this.jobDetailsContent.push({url: v.location, content: c})    
+                        }
+                    }
                     saveChanges = true
                 } 
             }
--- http-request.ts
+++ http-request.ts
@@ -1,8 +1,10 @@
 import * as https from 'https'
+import * as http from 'http'
 
-export function request(url: string, cookie: string = ''): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        https.get(url, { headers: {'cookie': cookie}}, response => {
+export function request(url: string, cookie: string = ''): Promise<string | {url: string, location: string, data: string}> {
+    return new Promise<string  | {url: string, location: string, data: string}>((resolve, reject) => {
+        
+        (url.startsWith('https://')  ? https : http).get(url, { headers: {'cookie': cookie}}, response => {
             let data = ''
         
             response.on('data', chunk => {
@@ -10,6 +12,10 @@ export function request(url: string, cookie: string = ''): Promise<string> {
             })
         
             response.on('end', () => {
+                if (response.statusCode === 302) {
+                    const location = response.headers['location']
+                    resolve({url, location, data})
+                }
                 resolve(data)
             })
         }).on('error', err => {
