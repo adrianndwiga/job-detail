bc740530966ba076094324f84737ef8fede8b285
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-03 10:59:45 +0000

    refactoring loading job details

--- load-job-details/index.ts
+++ load-job-details/index.ts
@@ -1,13 +1,13 @@
-import { readFileSync } from "fs"
+// import { readFileSync } from "fs"
 import * as cheerio from "cheerio"
 
 interface CssConfig {
     title: string
-    company: string
-    contact: string
+    company?: string
+    contact?: string
     description: string
-    location: string | string[]
-    salary: string
+    location?: string | string[]
+    salary?: string
 }
 
 interface JobDetailItem {
@@ -24,28 +24,38 @@ interface Job {
     salary: string
 }
 
-const jobs = JSON.parse(readFileSync('./data.json', 'utf8')) as JobDetailItem[]
-const config = JSON.parse(readFileSync('./load-job-details/config.json', 'utf8'))
+// const jobs = JSON.parse(readFileSync('./data.json', 'utf8')) as JobDetailItem[]
+// const config = JSON.parse(readFileSync('./load-job-details/config.json', 'utf8'))
 
-for(const job of jobs) {
-    // console.log(loadJob(job))
+// for(const job of jobs) {
+//     // console.log(loadJob(job))
+// }
+
+// function getCssConfig(item: JobDetailItem, ): CssConfig {
+//     for(const c in config)
+//         if (item.url.includes(c))
+//             return config[c] as CssConfig
+//     return undefined
+// }
+
+// function text($: any, css: CssConfig, selector: string): string {
+//     return css[selector] ? $(css[selector]).text()
+//             .replace(/\n/g, '')
+//             .replace(/\t/g, '')
+//             .trim() : ""
+// }
+
+function text(key: string | undefined, value: string): string {
+    if (key)
+        return value ? value
+                .replace(/\n/g, '')
+                .replace(/\t/g, '')
+                .trim() : ""
+    return ''
 }
 
-function getCssConfig(item: JobDetailItem, ): CssConfig {
-    for(const c in config)
-        if (item.url.includes(c))
-            return config[c]
-}
-
-function text($: any, css: CssConfig, selector: string): string {
-    return css[selector] ? $(css[selector]).text()
-            .replace(/\n/g, '')
-            .replace(/\t/g, '')
-            .trim() : ""
-}
-
-function loadJob(item: JobDetailItem): Job {
-    const css: CssConfig =  getCssConfig(item)
+export function loadJob(css: CssConfig, item: JobDetailItem): Job {
+    // const css: CssConfig =  getCssConfig(item)
     const $ = cheerio.load(item.content)
 
     let location = ''
@@ -66,11 +76,11 @@ function loadJob(item: JobDetailItem): Job {
 
     return {
         title: $(css.title).text().replace(/\n/g, '').replace(/\t/g, '').trim(),
-        description: $(css.description).html(),
-        company: text($, css, 'company'), // css.company ? $(css.company).html() : "",
-        contact: text($, css, 'contact').replace(/contact: /gi, ''),
+        description: $(css.description).html() as string,
+        company: text(css.company, $(css.company).text()), // css.company ? $(css.company).html() : "",
+        contact: text(css.contact, $(css.contact).text()).replace(/contact: /gi, ''),
         location,
-        salary: text($, css, 'salary')
+        salary: text(css.salary, $(css.salary).text())
     }
 }
 
