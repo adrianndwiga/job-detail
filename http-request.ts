25ce0ef23a7b663748702a99527b0d2e6357d27d
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-03 08:16:12 +0000

    fixing tslint errors

--- http-request.ts
+++ http-request.ts
@@ -3,23 +3,22 @@ import * as http from 'http'
 
 export function request(url: string, cookie: string = ''): Promise<string | {url: string, location: string, data: string}> {
     return new Promise<string  | {url: string, location: string, data: string}>((resolve, reject) => {
-        
+
         (url.startsWith('https://')  ? https : http).get(url, { headers: {'cookie': cookie}}, response => {
             let data = ''
-        
+
             response.on('data', chunk => {
                 data += chunk
             })
-        
+
             response.on('end', () => {
                 if (response.statusCode === 302) {
-                    const location = response.headers['location']
+                    const location = response.headers.location
                     resolve({url, location, data})
                 }
                 resolve(data)
             })
         }).on('error', err => {
-            console.log('error retrieving request')
             reject(`Error: ${err.message}`)
         })
     })
