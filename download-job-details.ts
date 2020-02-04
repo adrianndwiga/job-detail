50681db986ae59708513d27e1adebc382ddb5e6e
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-04 15:29:05 +0000

    removed commented out code

--- download-job-details.ts
+++ download-job-details.ts
@@ -1,39 +1,28 @@
-// import { readFileSync, existsSync, writeFileSync } from "fs"
 import { request } from "./http-request"
 
 export default class DownloadJobDetails {
 
-    // private readonly urls: string[]
-    // private readonly jobDetailsContent: {url: string, content: string}[]
-
-    constructor(private readonly urls: string[], private readonly jobDetailsContent: {url: string, content: string}[]) {
-        // this.urls = existsSync(jobUrlsFile) ? JSON.parse(readFileSync(jobUrlsFile, 'utf8')) : []
-        // this.jobDetailsContent = existsSync(jobDetailContentFile) ? JSON.parse(readFileSync(jobDetailContentFile, 'utf8')) : []
+    constructor(private readonly urls: string[], private readonly jobDetailsContent: { url: string, content: string }[]) {
     }
 
-    async load():  Promise<{url: string, content: string}[]> {
-        return new Promise<{url: string, content: string}[]>(async (resolve, reject) => {
-            // let saveChanges = false
-            for(const url of this.urls) {
+    async load(): Promise<{ url: string, content: string }[]> {
+        return new Promise<{ url: string, content: string }[]>(async (resolve, reject) => {
+            for (const url of this.urls) {
                 const jobDownloaded = this.jobDetailsContent.filter(x => x.url === url).length > 0
                 if (!jobDownloaded) {
                     const content = await request(url)
-                    if(typeof(content) === 'string')
-                        this.jobDetailsContent.push({url, content})
-                    else if (typeof(content) === 'object') {
-                        const v = content as {url: string, data: string, location: string}
+                    if (typeof (content) === 'string')
+                        this.jobDetailsContent.push({ url, content })
+                    else if (typeof (content) === 'object') {
+                        const v = content as { url: string, data: string, location: string }
                         if (!(this.jobDetailsContent.filter(x => x.url === v.location).length > 0)) {
                             const c = await request(v.location) as string
-                            this.jobDetailsContent.push({url: v.location, content: c})
+                            this.jobDetailsContent.push({ url: v.location, content: c })
                         }
                     }
-                    // saveChanges = true
                 }
             }
-            // if (saveChanges) {
-                resolve(this.jobDetailsContent)
-            // }
-            // resolve()
+            resolve(this.jobDetailsContent)
         })
     }
 }
