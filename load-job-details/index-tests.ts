4f58e508c153e2909de4565a68f409633736ec1d
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-07 10:04:50 +0000

    adding test to test job is loaded

--- load-job-details/index-tests.ts
+++ load-job-details/index-tests.ts
@@ -1,30 +1,55 @@
 import { loadJob } from "."
 import * as assert from 'assert'
 
-// tslint:disable-next-line: no-console
-console.log('.........')
-
 describe('load job', () => {
-    it('should load job details', () => () => {
-        const job = loadJob({
-            title: 'h1',
-            company: 'div.company',
-            contact: 'div.contact',
-            description: 'div.description',
-            location: 'div.location',
-            salary: 'div.salary'
-        }, {
-            url: '',
-            content: `<root>
-                <h1>some title</h1>
-                <div class="company">some company</div>
-                <div class="contact">some contact</div>
-                <div class="description">some description</div>
-                <div class="location">some location</div>
-                <div class="salary">some salary</div>
-            </root>`
-        })
 
-        assert.equal(job.location, 'some location')
+    describe('using css selector', () => {
+        it('should load job details', () => () => {
+            const job = loadJob({
+                title: 'h1',
+                company: 'div.company',
+                contact: 'div.contact',
+                description: 'div.description',
+                location: 'div.location',
+                salary: 'div.salary'
+            }, {
+                url: '',
+                content: `<root>
+                    <h1>some title</h1>
+                    <div class="company">some company</div>
+                    <div class="contact">some contact</div>
+                    <div class="description">some description</div>
+                    <div class="location">some location</div>
+                    <div class="salary">some salary</div>
+                </root>`
+            })
+
+            assert.equal(job.location, 'some location')
+        })
+    })
+
+    describe('using xpath selector', () => {
+        it('should load job details', () => () => {
+            const job = loadJob({
+                title: '//h1',
+                company: '//div[@class=company]',
+                contact: '//div[@class=contact]',
+                description: '//div[@class=description]',
+                location: '//div[@class=location]',
+                salary: '//div[@class=salary]'
+            }, {
+                url: '',
+                content: `<root>
+                    <h1>some title</h1>
+                    <div class="company">some company</div>
+                    <div class="contact">some contact</div>
+                    <div class="description">some description</div>
+                    <div class="location">some location</div>
+                    <div class="salary">some salary</div>
+                </root>`
+            })
+
+            assert.equal(job.location, 'some location')
+        })
     })
 })
