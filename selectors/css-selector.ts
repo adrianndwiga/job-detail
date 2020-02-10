6ab4e1801f06523838d18bc31600de29cf88cdbc
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-10 11:58:44 +0000

    refactoring

--- selectors/css-selector.ts
+++ selectors/css-selector.ts
@@ -13,9 +13,9 @@ export class CssSelector implements Selector {
         if (key) {
             const text = this.$(key).text()
             return text ? text
-                    .replace(/\n/g, '')
-                    .replace(/\t/g, '')
-                    .trim() : ""
+                .replace(/\n/g, '')
+                .replace(/\t/g, '')
+                .trim() : ""
         }
         return ''
     }
@@ -23,33 +23,38 @@ export class CssSelector implements Selector {
     title(): string {
         return this.text(this.css.title)
     }
+
     description(): string {
         return this.$(this.css.description).html() as string
     }
+
     company(): string {
         return this.text(this.css.company)
     }
+
     location(): string {
         let location = ''
-        if (typeof(this.css.location) === 'string') {
+        if (typeof (this.css.location) === 'string') {
             location = this.css.location ? this.$(this.css.location).text()
-            .replace(/\n/g, '')
-            .replace(/\t/g, '')
-            .trim() : ""
-        } else if(Array.isArray(this.css.location)) {
-            for(const l of this.css.location) {
+                .replace(/\n/g, '')
+                .replace(/\t/g, '')
+                .trim() : ""
+        } else if (Array.isArray(this.css.location)) {
+            for (const l of this.css.location) {
                 if (this.$(l).text()) {
                     location = this.$(l).text().replace(/\n/g, '')
-                    .replace(/\t/g, '')
-                    .trim()
+                        .replace(/\t/g, '')
+                        .trim()
                 }
             }
         }
         return location
     }
+
     contact(): string {
         return this.text(this.css.contact).replace(/contact: /gi, '')
     }
+
     salary(): string {
         return this.text(this.css.salary)
     }
